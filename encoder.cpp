5e74e0e33338eace7a91adf2c714bb71f0a8769f
rahulbatradwarka121-boop <rahul.batra.dwarka.121@gmail.com>
2026-07-02 16:51:20 +0530

    updated .gitignore

--- encoder.cpp
+++ encoder.cpp
@@ -2,33 +2,40 @@
 #include <queue>
 #include <unordered_map>
 #include <vector>
+#include <fstream>
 using namespace std;
 class node{
-    public:
+  public:
   node* left;
   node* right;
   char val ;
   int count;
   node(){
+    left = nullptr;
+    right = nullptr;
+    count = 0;
+    val = '?';
   }
   node(int cnt , char c)
   {
-      this->left = NULL;
-      this->right = NULL;
+      this->left = nullptr;
+      this->right = nullptr;
       this->count = cnt;
       this->val = c; 
   }
+  
 };
 class comp{
     public:
-  bool operator()(node*&a , node*&b)
+  bool operator()(node* a , node* b)
   {
       return a->count > b->count;
   }
 };
-class hoffman{
+class Huffman{
     public:
     unordered_map<char , string>mp;
+    node* root = nullptr;
     priority_queue<node* , vector<node*> , comp>pq;
     void push(char val , int count)
     {
@@ -37,11 +44,11 @@ class hoffman{
     }
     void fill_map(node* root , string code)
     {
-        if(root == NULL)
+        if(root == nullptr)
         {
             return;
         }
-        if(root->left == NULL && root->right == NULL)
+        if(root->left == nullptr && root->right == nullptr)
         {
             mp[root->val] = code;
         }
@@ -55,6 +62,16 @@ class hoffman{
         }
     }
     void fill(){
+        if(pq.empty())
+        {
+            return;
+        }
+        if(pq.size() == 1)
+        {
+            root = pq.top();
+            mp[pq.top()->val] = "0";
+            return;
+        }
         while(pq.size() != 1)
         {
             node* first = pq.top();
@@ -67,24 +84,76 @@ class hoffman{
             newNode->right = second;
             pq.push(newNode);
         }
-        fill_map(pq.top() , "");
+        
+        root = pq.top();
+        fill_map(root , "");
     }
-    void print(){
-        for(auto it : mp)
+    void delete_Nodes(node* root){
+        if(root == nullptr)
         {
-            cout<<it.first<<" "<<it.second<<endl;
+            return ;
+        }
+        delete_Nodes(root -> left);
+        delete_Nodes(root -> right);
+        delete root;
+    }
+    void encode( ifstream &input , ofstream &output ,ofstream &meta , const unordered_map<char,int>& freq){
+        char ch;
+
+        while(input.get(ch))
+        {
+            output << mp[ch];
+        }
+        for(const auto& it : freq)
+        {
+            meta << (int)it.first << " "<< it.second << "\n";
         }
     }
+    ~Huffman(){
+            if(!pq.empty())
+            {
+                delete_Nodes(pq.top());
+            }
+        }
 };
 int main(){
-    hoffman hm;
-    hm.push('a' , 5);
-    hm.push('b' , 9);
-    hm.push('c' , 12);
-    hm.push('d' , 13);
-    hm.push('e' , 16);
-    hm.push('f' , 45);
+    ifstream input("input.txt");
+    if(!input.is_open())
+    {
+        cout << "Failed to open file\n";
+        return 0;
+    }
+    char ch;
+    unordered_map<char,int> freq;
+    while(input.get(ch))
+    {
+        freq[ch]++;
+    }
+    input.close();
+    Huffman hm;
+    for(const auto &it : freq)
+    {
+        hm.push(it.first , it.second);
+    }
     hm.fill();
-    hm.print();
+    input.open("input.txt");
+    if(!input.is_open())
+    {
+        cout << "Failed to reopen file\n";
+        return 0;
+    }
+    ofstream output("output.txt");
+    if(!output.is_open())
+    {
+        cout << "Failed to create output file\n";
+        return 0;
+    }
+    ofstream meta("meta.txt");
+    if(!meta.is_open())
+    {
+        cout << "Failed to create output file Transformer\n";
+        return 0;
+    }
+    hm.encode(input , output , meta , freq);
     return 0;
 }
